b0fe8fe9e0b26d2f1de51c565b57f0f24c07f6b4
pouria <poria.k1990@gmail.com>
2023-10-06 18:44:27 +0330

    set page scroll and ...

--- scrll/src/app/app-routing.module.ts
+++ scrll/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'scroll-1',
+    loadChildren: () => import('./modules/scroll-1/scroll-1.module').then(m => m.Scroll1Module),
+   
+},
+{
+  path: 'scroll-2',
+  loadChildren: () => import('./modules/scroll-2/scroll-2.module').then(m => m.Scroll2Module),
+ 
+},
+{ path: '',   redirectTo: 'scroll-1/test-scroll-1', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- scrll/src/app/app.module.ts
+++ scrll/src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { FormsModule } from '@angular/forms';
+import { VirtualScrollerDefaultOptions } from 'ngx-virtual-scroller';
+export function vsDefaultOptionsFactory(): VirtualScrollerDefaultOptions {
+  return {
+      checkResizeInterval: 1000,
+      modifyOverflowStyleOfParentScroll: true,
+      resizeBypassRefreshThreshold: 5,
+      scrollAnimationTime: 750,
+      scrollDebounceTime: 0,
+      scrollThrottlingTime: 0,
+      stripedTable: false
+  };
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ {provide: 'virtual-scroller-default-options', useFactory: vsDefaultOptionsFactory}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
